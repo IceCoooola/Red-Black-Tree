aa6bd3adffc4cd4451a4e3b3bae9aceddddf812b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-10-20 17:01:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include"RBTree.h"
 
 int main() {
     RBTree<int, int> rb;
-    std::vector<int> v = {1,5,74,-34,235,6,8,344,213,0};
+    std::vector<int> v = {1,5,74,-34,235,6,8,344,213,0,-1,-2,-3,-4,-5};
     for(int i = 0; i < v.size(); i++)
     {
         rb.insert(std::make_pair(v[i],v[i]));
     }
     rb.PreOrder();
+    cout<<rb.isBalance()<<std::endl;
     return 0;
 }
